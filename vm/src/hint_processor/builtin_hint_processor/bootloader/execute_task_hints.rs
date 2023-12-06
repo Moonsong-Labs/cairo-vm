38a5a574f29ceae1824c6e5fb6ff015418fbf360
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-12-06 12:10:12 +0100

    fixes for review

--- vm/src/hint_processor/builtin_hint_processor/bootloader/execute_task_hints.rs
+++ vm/src/hint_processor/builtin_hint_processor/bootloader/execute_task_hints.rs
@@ -11,7 +11,7 @@ use crate::vm::vm_core::VirtualMachine;
 /// Implements %{ ids.program_data_ptr = program_data_base = segments.add() %}.
 ///
 /// Creates a new segment to store the program data.
-pub fn set_program_data_ptr(
+pub fn allocate_program_data_segment(
     vm: &mut VirtualMachine,
     exec_scopes: &mut ExecutionScopes,
     ids_data: &HashMap<String, HintReference>,
@@ -41,17 +41,18 @@ mod tests {
     use super::*;
 
     #[rstest]
-    fn test_set_program_data_ptr() {
+    fn test_allocate_program_data_segment() {
         let mut vm = vm!();
         // Allocate space for program_data_ptr
         vm.run_context.fp = 1;
         add_segments!(vm, 2);
         let ids_data = ids_data!["program_data_ptr"];
+        let expected_program_data_segment_index = vm.segments.num_segments();
 
         let mut exec_scopes = ExecutionScopes::new();
         let ap_tracking = ApTracking::new();
 
-        set_program_data_ptr(&mut vm, &mut exec_scopes, &ids_data, &ap_tracking)
+        allocate_program_data_segment(&mut vm, &mut exec_scopes, &ids_data, &ap_tracking)
             .expect("Hint failed unexpectedly");
 
         let program_data_ptr =
@@ -63,5 +64,17 @@ mod tests {
             .expect(format!("{} is not set", vars::PROGRAM_DATA_BASE).as_ref());
 
         assert_eq!(program_data_ptr, program_data_base);
+        // Check that we allocated a new segment and that the pointers point to it
+        assert_eq!(
+            vm.segments.num_segments(),
+            expected_program_data_segment_index + 1
+        );
+        assert_eq!(
+            program_data_ptr,
+            Relocatable {
+                segment_index: expected_program_data_segment_index as isize,
+                offset: 0
+            }
+        );
     }
 }
